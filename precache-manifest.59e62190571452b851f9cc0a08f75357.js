d9f38ca521395b2d8b8a13e69bb5d970f5e925c0
ivoelbert <ivo.elbert1@gmail.com>
2020-05-02 20:58:13 -0300

    Updates

---
+++ precache-manifest.59e62190571452b851f9cc0a08f75357.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19cc76a6540feef1094907760905b9a1",
+    "url": "/poly-remake/index.html"
+  },
+  {
+    "revision": "52fa27877279ea10ef39",
+    "url": "/poly-remake/static/css/main.3dcb7389.chunk.css"
+  },
+  {
+    "revision": "fc56e77b05f4cca81ccc",
+    "url": "/poly-remake/static/js/2.49cf8d63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/poly-remake/static/js/2.49cf8d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52fa27877279ea10ef39",
+    "url": "/poly-remake/static/js/main.416ab62c.chunk.js"
+  },
+  {
+    "revision": "2b31afe7d8a78dc89907",
+    "url": "/poly-remake/static/js/runtime-main.d4fa136c.js"
+  }
+]);
